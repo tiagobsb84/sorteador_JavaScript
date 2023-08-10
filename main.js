feb7c9f849db91373a91b80167abf39c63dc2fd0
Tiago Silva <tiagobsb84@gmail.com>
2023-08-10 10:03:17 -0300

    alterando texto para receber o resultado

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ function sortear() {
 const min = Math.ceil(document.querySelector(".first-value").value);
 const max = Math.floor(document.querySelector(".second-value").value);
 
+const textResult = document.querySelector(".title-content");
+
 let randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
 
-alert(randomValue)
+textResult.innerHTML = `O número sorteado foi:  ${randomValue}`;
 }
 
 buttonRaffle.addEventListener("click", sortear);
